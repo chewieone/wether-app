421d2c53e9eea7528382334beb797634e4b719f2
chewieone <himavanth.goud.maregoni@gmail.com>
2026-02-03 16:10:52 +0530

    Updated weather app UI and animations

--- script.js
+++ script.js
@@ -1,21 +1,65 @@
-function displayInput(){
+// =========== Taking input ==========
 
-    let inputEle=document.getElementById("finder");
-    let input=inputEle.value;
-    console.log(input);
-   let locationEle=document.querySelector(".location");
-   input=input.trim();
-   inputEle.value="";
-   locationEle.textContent=input;
-   console.log(locationEle.innerText)
-}
-let inputEle=document.getElementById("finder");
-inputEle.addEventListener("keydown",function(event){
-    if (event.key=="Enter"){
-        displayInput();
-    }
-});
+const inputElem = document.querySelector("#finder");
+const buttonElem = document.querySelector("#button");
 
-document.getElementById("button")
-.addEventListener("click",displayInput)
+buttonElem.addEventListener("click", ()=> {
+    const location = inputElem.value.trim();
+
+    //call the API -
+    
+fetchWeather(location).then((data)=>{
+
+    console.log(data)
+    updateDom(data)
+
+} )
+
+
+    //update the DOM
+
+
+
+    inputElem.value="";
+})
+
+    //     Promises
+         async function fetchWeather(location) {
+        const link =`https://api.weatherapi.com/v1/current.json?key=c7236d36debb4636a18170654262201&q=${location}&aqi=no`
+        const response = await fetch(
+          link,
+        );
+        console.log(response);
+        const jsonData = await response.json();
+       
+        return jsonData;
+      }
+        function updateDom(data){
+            //filter data
+            //1 getting temp
+           const temp=data.current.temp_c
+            const lcn=data.location.name
+            const WetherCond=data.current.condition.text
+           const timeline=data.location.localtime
+           const [date,time]=timeline.split(" ")
+           const iconUrl=data.current.condition.icon
+
+           //***************update dom ***************/
+           //select ele
+           const tempEle=document.querySelector(".temperature");
+           const locEle=document.querySelector(".location");
+           const imgUrl=document.querySelector(".image");
+            const cdnEle=document.querySelector(".condition");
+            const dayele=document.querySelector(".date")
+            const timeEle=document.querySelector(".time")
+           //update
+           tempEle.textContent=temp+"°C"
+           locEle.textContent=lcn
+           imgUrl.src=iconUrl
+           cdnEle.textContent=WetherCond
+            dayele.textContent=date
+            timeEle.textContent=time
+        }
+
+
 
